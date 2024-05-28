e60632197bf36a3b4ccf3cf81781764447f14aec
Arian Salimy <asalimy@uci.edu>
2024-05-28 16:20:33 -0700

    more blue

--- src/app/component/navbar.tsx
+++ src/app/component/navbar.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 bg-[#1b3d6d] sticky top-0">
 
         <div className="container mx-auto px-4 h-full">
 
