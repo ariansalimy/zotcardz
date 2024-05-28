fbb3c6697e9931e5316ddc3f058512f4b5db3a58
Arian Salimy <asalimy@uci.edu>
2024-05-28 16:19:27 -0700

    to zot with love..

---
+++ src/app/component/navbar.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Link from "next/link";
+
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+
+        <div className="container mx-auto px-4 h-full">
+
+          <div className="flex justify-center items-center h-full">
+          
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>Find</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/services">
+                  <p>Saved</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <p>Create</p>
+                </Link>
+              </li>
+            </ul>
+            
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
