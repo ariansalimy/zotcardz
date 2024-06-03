a9f461bd46626e8da94493912a55c6efc61b4490
Arian Salimy <asalimy@uci.edu>
2024-06-03 14:29:47 -0700

    drag?

---
+++ src/app/component/droppable.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {useDroppable} from '@dnd-kit/core';
+
+function Droppable(props: { children: string | number | bigint | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<React.AwaitedReactNode> | null | undefined; }) {
+  const {isOver, setNodeRef} = useDroppable({
+    id: 'droppable',
+  });
+  const style = {
+    color: isOver ? 'green' : undefined,
+  };
+  
+  
+  return (
+    <div ref={setNodeRef} style={style}>
+      {props.children}
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
 import Image from "next/image";
 import Navbar from "./component/navbar";
+import { DndContext } from "@dnd-kit/core";
+import { useState } from "react";
+
 export default function Home() {
+  const [isDropped, setIsDropped] = useState(false);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
-      <div className=" w-full   justify-between ">
+      <div className=" w-full  justify-between ">
         <Navbar></Navbar>
       </div>
+
     </main>
   );
 }
