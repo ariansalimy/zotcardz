726a76afa2963d7228b5a2271c85bdaf820b354f
Arian Salimy <asalimy@uci.edu>
2024-06-04 16:07:51 -0700

    some css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,31 +12,31 @@ export default function Home() {
   const [parent, setParent] = useState(null);
 
   const draggableMarkup = (
-    <Draggable id="draggable"><ExCard/></Draggable>
+    <Draggable id="d1"><ExCard/></Draggable>
   );
   return (
     <main className=" min-h-screen ">
       <div className=" w-full flex flex-col items-center  ">
         <Navbar></Navbar>
       </div>
-      <div className="p-12 flex justify-center space-x-3 flex-row">
+      <div className="p-8 flex justify-center space-x-3 flex-row">
 
    
       <DndContext onDragEnd={handleDragEnd}>
-    <div className='items-center flex flex-col border-2 size-40'>
+    <div className='items-center flex flex-col border-2 w-4/6 h-96'>
       To save
       {parent === null ? draggableMarkup : null}
     </div>
      
    
-    <div className='flex flex-col items-center border-2 size-40'>
+    <div className='flex flex-col items-center border-2 w-2/6 '>
       Saved
       {containers.map((id) => (
         // We updated the Droppable component so it would accept an `id`
         // prop and pass it to `useDroppable`
        
         <Droppable key={id} id={id}>
-          {parent === id ? draggableMarkup : <div className="size-40"></div>}
+          {parent === id ? draggableMarkup : <div className="bg-amber-200 w-96 h-96"></div>}
         </Droppable>
 
      
