7650c67ed8cba88212bc186da831db23fe038f94
Jesse David <114846268+jessed7@users.noreply.github.com>
2024-06-04 18:32:44 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,59 +1,60 @@
-"use client"
+"use client";
 import Image from "next/image";
 import Navbar from "./component/navbar";
 import Event_card from "./component/Event_card";
 import Save_button from "./component/Save_button";
 import { DndContext } from "@dnd-kit/core";
 import { useState } from "react";
-import {Draggable} from "./component/draggable"
-import {Droppable} from './component/droppable';
+import { Draggable } from "./component/draggable";
+import { Droppable } from "./component/droppable";
 import { ExCard } from "./component/excard";
 
+import { CardData, months, min_to_hrmin, hrmin_to_min } from "./data/card-data";
+
 export default function Home() {
-  const containers = ['A']
+  const containers = ["A"];
   const [parent, setParent] = useState(null);
 
   const draggableMarkup = (
-    <Draggable id="d1"><Event_card></Event_card></Draggable>
+    <Draggable id="d1">
+      <Event_card></Event_card>
+    </Draggable>
   );
 
   const events_section = {
     display: "flex",
     justifyContent: "center",
     backgroundColor: "#6AA2B8",
-    padding: '10px 10px',
+    padding: "10px 10px",
     width: "1128px",
     height: "500px",
-    borderRadius: '16px',
-    
-  }
+    borderRadius: "16px",
+  };
 
   const save_section = {
     display: "flex",
     justifyContent: "center",
     backgroundColor: "#6AA2B8",
-    padding: '10px 10px',
+    padding: "10px 10px",
     width: "1128px",
     height: "300px",
-    borderRadius: '16px',
-  }
+    borderRadius: "16px",
+  };
 
   const header = {
     color: "white",
     fontWeight: "bold",
     fontSize: "24px",
-  }
+  };
 
   const container = {
     display: "grid",
     gap: "10px",
-}
+  };
 
   const item = {
     padding: "3px",
-  }
-
-  
+  };
 
   return (
     <main className=" min-h-screen ">
@@ -63,70 +64,78 @@ export default function Home() {
 
       {/*drag and drop section*/}
       <div style={container}>
-      <div style={item}>
-      <div className="p-8 flex justify-center space-x-3 flex-row">
+        <div style={item}>
+          <div className="p-8 flex justify-center space-x-3 flex-row">
+            <DndContext onDragEnd={handleDragEnd}>
+              <div className="items-center flex flex-col border-2 w-4/6 h-96">
+                To save
+                {parent === null ? draggableMarkup : null}
+              </div>
 
-   
-      <DndContext onDragEnd={handleDragEnd}>
-    <div className='items-center flex flex-col border-2 w-4/6 h-96'>
-      To save
-      {parent === null ? draggableMarkup : null}
-    </div>
-     
-   
-    <div className='flex flex-col items-center border-2 w-2/6 '>
-      Saved
-      {containers.map((id) => (
-        // We updated the Droppable component so it would accept an `id`
-        // prop and pass it to `useDroppable`
-       
-        <Droppable key={id} id={id}>
-          {parent === id ? draggableMarkup : <div className="bg-amber-200 w-96 h-96"></div>}
-        </Droppable>
+              <div className="flex flex-col items-center border-2 w-2/6 ">
+                Saved
+                {containers.map((id) => (
+                  // We updated the Droppable component so it would accept an `id`
+                  // prop and pass it to `useDroppable`
 
-     
-      ))}
-           </div>
-    </DndContext>
-    </div>
-    
-
-        {/*events section*/}
-        <div className="flex justify-center p-10">
-        <div style={events_section}>
-          <div style={container}>
-
-          <h1 style={header}>UCI Events</h1>
-          
-          <div style={container}>
-            <div style={item}><Event_card></Event_card></div>
-            <div style={item}><Event_card></Event_card></div>
+                  <Droppable key={id} id={id}>
+                    {parent === id ? (
+                      draggableMarkup
+                    ) : (
+                      <div className="bg-amber-200 w-96 h-96"></div>
+                    )}
+                  </Droppable>
+                ))}
+              </div>
+            </DndContext>
           </div>
 
-          </div>
-        </div>
-        </div>
+          {/*events section*/}
 
+          <DndContext onDragEnd={handleDragEnd}>
+
+          <div className="flex justify-center p-10">
+            <div style={events_section}>
+              <div style={container}>
+                <h1 style={header}>UCI Events</h1>
+
+                <div style={container}>
+                  <div style={item}>
+                    <Draggable id='card1'>
+                    <Event_card></Event_card>
+
+                    </Draggable>
+                  </div>
+                  <div style={item}>
+                    <Event_card></Event_card>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        <Droppable id={"saving"}>
         <div className="flex justify-center p-3">
-          <div style={save_section}>
-            <div style={container}>
-            <h1 style={header}>Events to Save</h1>
-            <div className="flex justify-center p-3">
-            <Save_button></Save_button>
+            <div style={save_section}>
+              <div style={container}>
+                <h1 style={header}>Events to Save</h1>
+                <div className="flex justify-center p-3">
+                  <Save_button></Save_button>
+                </div>
+              </div>
             </div>
-            </div>
-            
-            
-            
           </div>
-        </div>
+        </Droppable>
+          
+            </DndContext>
 
-  </div>
-  </div>
+
+        </div>
+      </div>
     </main>
   );
-  function handleDragEnd(event:any) {
-    const {over} = event;
+  function handleDragEnd(event: any) {
+    const { over } = event;
 
     // If the item is dropped over a container, set it as the parent
     // otherwise reset the parent to `null`
