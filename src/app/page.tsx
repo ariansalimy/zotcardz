8ac322c4a9c573a8ed1bb4ac4c568571dfc7683b
Amy Fong <amyf3@uci.edu>
2024-06-04 18:00:12 -0700

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import Image from "next/image";
 import Navbar from "./component/navbar";
+import Event_card from "./component/Event_card";
+import Save_button from "./component/Save_button";
 import { DndContext } from "@dnd-kit/core";
 import { useState } from "react";
 import {Draggable} from "./component/draggable"
@@ -14,11 +16,54 @@ export default function Home() {
   const draggableMarkup = (
     <Draggable id="d1"><ExCard/></Draggable>
   );
+
+  const events_section = {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#6AA2B8",
+    padding: '10px 10px',
+    width: "1128px",
+    height: "500px",
+    borderRadius: '16px',
+    
+  }
+
+  const save_section = {
+    display: "flex",
+    justifyContent: "center",
+    backgroundColor: "#6AA2B8",
+    padding: '10px 10px',
+    width: "1128px",
+    height: "300px",
+    borderRadius: '16px',
+  }
+
+  const header = {
+    color: "white",
+    fontWeight: "bold",
+    fontSize: "24px",
+  }
+
+  const container = {
+    display: "grid",
+    gap: "10px",
+}
+
+  const item = {
+    padding: "3px",
+  }
+
+  
+
   return (
     <main className=" min-h-screen ">
       <div className=" w-full flex flex-col items-center  ">
         <Navbar></Navbar>
       </div>
+
+      {/*drag and drop section*/}
+      <div style={container}>
+      <div style={item}>
       <div className="p-8 flex justify-center space-x-3 flex-row">
 
    
@@ -43,7 +88,40 @@ export default function Home() {
       ))}
            </div>
     </DndContext>
+    </div>
+    
 
+        {/*events section*/}
+        <div className="flex justify-center p-10">
+        <div style={events_section}>
+          <div style={container}>
+
+          <h1 style={header}>UCI Events</h1>
+          
+          <div style={container}>
+            <div style={item}><Event_card></Event_card></div>
+            <div style={item}><Event_card></Event_card></div>
+          </div>
+
+          </div>
+        </div>
+        </div>
+
+        <div className="flex justify-center p-3">
+          <div style={save_section}>
+            <div style={container}>
+            <h1 style={header}>Events to Save</h1>
+            <div className="flex justify-center p-3">
+            <Save_button></Save_button>
+            </div>
+            </div>
+            
+            
+            
+          </div>
+        </div>
+
+  </div>
   </div>
     </main>
   );
