9bdf3865da1883e3dc2912cde98a71cb8473b95f
Jesse David <114846268+jessed7@users.noreply.github.com>
2024-06-05 14:10:15 -0700

    Added card info modal to Saved page

--- src/app/saved.tsx
+++ src/app/saved.tsx
@@ -6,8 +6,11 @@ import Event_card from "./component/Event_card";
 import { Dispatch, SetStateAction, useState } from "react";
 import { Droppable } from "./component/droppable";
 import Save_button from "./component/Save_button";
-import { CardData } from "./data/card-data";
+import { CardData, sampleCard } from "./data/card-data";
 import CardList from "./component/card-list";
+import InfoModal from "./component/info-modal";
+import getSampleEvents from "./data/sampleEvents";
+
 
 type SavedProps = {
   unsavedEvents: Record<string, CardData>;
@@ -26,14 +29,44 @@ export default function Saved({
   savedEvents,
   setSavedEvents,
 }: SavedProps) {
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentCardModal, setCurrentCardModal] =
+    useState<CardData>(sampleCard);
+
+  function handleDragEnd(event: any) {
+    console.log(event);
+    const { active, over, delta } = event;
+
+    // If the item is dropped over a container, set it as the parent
+    // otherwise reset the parent to `null`
+    // setParent(over ? over.id : null);
+    console.log(active);
+    console.log(over);
+    // If delta.x and delta.y, then do not consider it a drag event
+    if ((delta.x === 0) && (delta.y === 0)) {
+        setCurrentCardModal(getSampleEvents()[active.id]);
+        setModalVisible(true);
+    }
+  }
+
   return (
-    <div className="flex flex-col items-center my-8">
-      <div className="flex flex-col items-center bg-blue-light w-11/12 rounded-2xl p-2.5 gap-4">
-        <h1 className="text-white font-bold text-2xl">Saved Events</h1>
-        <div className="flex justify-center gap-3 flex-wrap min-h-usah">
-          <CardList cards={savedEvents}></CardList>
+    <>
+      <DndContext onDragEnd={handleDragEnd}>
+        <div className="flex flex-col items-center my-8">
+          <div className="flex flex-col items-center bg-blue-light w-11/12 rounded-2xl p-2.5 gap-4">
+            <h1 className="text-white font-bold text-2xl">Saved Events</h1>
+            <div className="flex justify-center gap-3 flex-wrap min-h-usah">
+              <CardList cards={savedEvents}></CardList>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      </DndContext>
+
+      <InfoModal
+        cardData={currentCardModal}
+        visible={modalVisible}
+        setVisible={setModalVisible}
+      ></InfoModal>
+    </>
   );
 }
